9c18bd3e55873d0b4360f2376f71d48385ba55b8
clementdevarieux <devarieux.clement@gmail.com>
2023-05-05 16:45:14 +0200

    feature: first commit

---
+++ MyBear.py
@@ -0,0 +1,8 @@
+import numpy as np
+import test
+
+def main():
+    print(test.somme(4, 7))
+
+if __name__ == "__main__" :
+    main()
---
+++ test.py
@@ -0,0 +1,8 @@
+
+def somme(x, y):
+    return x + y
+
+
+if __name__ == "__main__" :
+    print("okok")
+    print(somme(2, 5))
