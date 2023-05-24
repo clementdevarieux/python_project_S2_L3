ab41f5727daeb4177e84a98a169f11955ee4bb07
clementdevarieux <devarieux.clement@gmail.com>
2023-05-24 20:21:58 +0200

    features: Classe Series first working implementation

--- MyBear.py
+++ MyBear.py
@@ -22,7 +22,14 @@ import test
 import series
 
 def main():
-    print(test.somme(4, 7))
+    serie_test = series.Series("Test", [1, 2, 3, 4, 5])
+    serie_test.print_series()
+    value_1 = serie_test.iloc[1]
+    serie_1 = serie_test.iloc[2:7]
+    serie_2 = serie_test.iloc[1:3]
+    print(f'iloc[3] vaut {value_1}')
+    print(f'iloc[2:7] vaut {serie_1}')
+    print(f'iloc[1:3] vaut {serie_2}')
 
 if __name__ == "__main__" :
     main()
--- series.py
+++ series.py
@@ -29,7 +29,7 @@ class Series:
       """Defines the maximum of this series.
       """
       max = self.values[0]
-      for value in values :
+      for value in self.values :
          if value > max :
             max = value
       return max
@@ -38,7 +38,7 @@ class Series:
       """Defines the minimum of this series.
       """
       min = self.values[0]
-      for value in values:
+      for value in self.values:
          if value < min:
             min = value
       return min
@@ -47,32 +47,53 @@ class Series:
       """Defines the mean of this series.
       """
       somme = 0
-      for value in values:
+      for value in self.values:
          somme += value
-      mean = round((somme / len(values)), 2)
+      mean = round((somme / len(self.values)), 2)
       return mean
 
    def _ourailleur (self) -> None:
       """Defines the standard deviation of this series.
       """
-      return np.std(values)
+      return np.std(self.values)
 
    def _ignition (self) -> None:
       """Defines the number of values of this series.
       """
-      return len(values)
-   
-   @property
-   def iloc(self) -> Any | Series:
-      return self._iloc
+      return len(self.values)
 
-   @iloc.setter
-   def iloc(self, *args):
-      if type(args[0]) == 'int':
-         return self.Series(args[0])
-      else :
-         #mettre en forme pour retourner les lignes voulues
-         pass
+   def print_series(self) -> None:
+      print(f'self.name = {self.name}')
+      print(f'self.values = {self.values}')
+      print(f'self.max = {self._lepard()}')
+      print(f'self.min = {self._note()}')
+      print(f'self.mean = {self._tones()}')
+      print(f'self.std = {self._ourailleur()}')
+      print(f'self.count = {self._ignition()}')
+
+   @property
+   def iloc(self) :
+      class IlocAccessor :
+         def __init__(self, values):
+            self.values = values
+
+         def __getitem__(self, item):
+            if isinstance(item, int):
+               return self.values[item]
+            elif type(item) == slice:
+               start, stop, step = item.indices(len(self.values))
+               print(f"start = {start}")
+               print(f"stop = {stop}")
+               print(f"step = {step}")
+               if stop == len(self.values):
+                  return [self.values[i] for i in range(start, stop, step)]
+               else :
+                  return [self.values[i] for i in range(start, stop + 1, step)]
+            else :
+               raise TypeError("Mauvais format dans iloc")
+
+      return IlocAccessor(self.values)
+
 
 
 
