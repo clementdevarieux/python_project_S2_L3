20eff10ba9d221f49f26b9f1bb63fff96bb918c9
Elbub <elbub.gamer@laposte.net>
2023-05-31 16:24:14 +0200

    Feature : added ways to test the join dataset method

--- MyBear.py
+++ MyBear.py
@@ -23,17 +23,17 @@ import series
 import dataframe
 
 def main():
-    serie_test_1 = series.Series("Test 1", ["hellof"])
-    serie_test_2 = series.Series("Test 2", ["1.5"])
-    serie_list = [serie_test_1, serie_test_2]
+    # serie_test_1 = series.Series("Test 1", ["hellof"])
+    # serie_test_2 = series.Series("Test 2", ["1.5"])
+    # serie_list = [serie_test_1, serie_test_2]
     #
     # # value_1 = serie_test.iloc[1]
     # # serie_test.print_series()
     # # print(f"iloc[1]= {value_1}")
     # df_test = dataframe.DataFrame(column_names=["test_1", "test2"], values=[[1,2],[3,4]])
     #
-    df_test_series = dataframe.DataFrame(serie_list=serie_list)
-    df_test_series.print_df()
+    # df_test_series = dataframe.DataFrame(serie_list=serie_list)
+    # df_test_series.print_df()
     # #df = df_test._count()
     # df = df_test._max()
     # df_2 = df_test_series._std()
@@ -47,6 +47,24 @@ def main():
     # print("json")
     # df_json.print_df_values()
 
+    left = dataframe.read_csv("join_test_left.csv")
+    right = dataframe.read_csv("join_test_right.csv")
+    left.print_as_table()
+    print("------")
+    right.print_as_table()
+    print("------")
+    patate = left.join(other = right, left_on = "EMPDEPT", right_on = "DEPTNAME", how = "inner")
+    patate.print_as_table()
+    print("------")
+    patate = left.join(other = right, left_on = "EMPDEPT", right_on = "DEPTNAME", how = "left")
+    patate.print_as_table()
+    print("------")
+    patate = left.join(other = right, left_on = "EMPDEPT", right_on = "DEPTNAME", how = "right")
+    patate.print_as_table()
+    print("------")
+    patate = left.join(other = right, left_on = "EMPDEPT", right_on = "DEPTNAME", how = "outer")
+    patate.print_as_table()
+    
 
 
 if __name__ == "__main__" :
