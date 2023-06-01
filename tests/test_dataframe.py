0f0ad0bc2b413703ea69d70052eb2e249b6804b5
Badr Bouaissa <badrboy45@gmail.com>
2023-06-01 11:22:59 +0200

    feature(tests): test dataframe added

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -11,11 +11,14 @@ def dataframe():
 def test_dataframe_is_dataframe_class(dataframe):
     assert isinstance(dataframe, DataFrame) == True
 
+
 def test_dataframe_i_loc_n_n(dataframe):
     assert dataframe.iloc[1, 1] == 5
 
+
 def test_dataframe_i_loc_a_b_n(dataframe):
     assert isinstance(dataframe.iloc[0:1, 1], Series) == True
+
 
 def test_read_csv_to_df():
     df_csv = read_csv("files/test_csv.csv")
@@ -25,6 +28,7 @@ def test_read_csv_to_df():
 def test_read_json_to_df():
     df_json = read_json("files/test_json.json")
     assert isinstance(df_json, DataFrame) == True
+
 
 def test_df_max(dataframe):
     df_max = dataframe._max()
@@ -63,8 +67,10 @@ def test_df_not_right_size():
             values=[[11, 21, 3, 4], [3, -4, 1, 9]],
         )
 
+
 def test_dataframe_i_loc_n_a_b(dataframe):
     assert isinstance(dataframe.iloc[1, 0:1], DataFrame) == True
+
 
 def test_dataframe_i_loc_x_y_a_b(dataframe):
     assert isinstance(dataframe.iloc[0:1, 0:1], DataFrame) == True
